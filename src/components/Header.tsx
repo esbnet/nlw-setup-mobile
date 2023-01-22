9aef016d993e2d1812418b787db17839f9198631
Edmilson Soares <esbnet@gmail.com>
2023-01-21 22:59:36 -0300

    feat: :sparkles: create scree add new habit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import colors from 'tailwindcss/colors';
 
 import Logo from '../assets/logo.svg';
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className='w-full flex-row items-center justify-between'>
 
@@ -14,6 +16,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.5}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center hover:border-violet-300"
+        onPress={() => navigate('new')}
       >
 
         <Feather
--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,9 +1,74 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { BackButton } from '../components/BackButton';
+import { Checkbox } from '../components/Checkbox';
+
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
+
+const avaliableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado',]
 
 export function New() {
+
+    const [weekDays, setWeekDays] = useState<number[]>([])
+
+    function hundleToggleWeekDay(weekDayIndex: number) {
+        if (weekDays.includes(weekDayIndex)) {
+            setWeekDays(weekDays.filter(day => day !== weekDayIndex))
+        } else {
+            setWeekDays(prevState => [...prevState, weekDayIndex])
+        }
+    }
+
     return (
         <View className='flex-1 bg-background px-8 pt-16'>
-            New
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
+            >
+                <BackButton />
+
+                <Text className='mt-4 text-white font-extrabold text-3xl'>
+                    Criar Hábit
+                </Text>
+                <Text className='mt-4 text-white font-semibold text-base'>
+                    Qual seu comprometimento?
+                </Text>
+
+                <TextInput
+                    className='h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white focus:border-2 border-yellow-300'
+                    placeholder='Ex: Fazer exercícios; Dormir bem; etc...'
+                    placeholderTextColor={colors.zinc[700]}
+                />
+
+                <Text className='font-semibold mt-4 mb-3 text-white text-base'>
+                    Qual a recorrência?
+                </Text>
+
+                {avaliableWeekDays.map((weekDay, index) => (
+                    <Checkbox
+                        key={weekDay}
+                        title={weekDay}
+                        checked={weekDays.includes(index)}
+                        onPress={() => hundleToggleWeekDay(index)} />
+                ))
+                }
+
+                <TouchableOpacity
+                    className='w-full h-14 flex-row items-center justify-center bg-yellow-600 rounded-md mt-6'
+                >
+                    <Feather
+                        name='plus'
+                        size={24}
+                        color={colors.white}
+                    />
+                    <Text className='font-semibold text-base text-white ml-2'>
+                        Confirmar
+                    </Text>
+                </TouchableOpacity>
+            </ScrollView>
         </View>
     )
+
 }
